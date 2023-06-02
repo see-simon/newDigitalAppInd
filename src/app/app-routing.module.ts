5b55f8c09f2995f6eb5b7253e9898ca089fca933
Dam197 <dam197@DA-MACs-MacBook-Air97.local>
2023-06-02 11:58:36 +0200

    files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { BreakefastComponent } from './breakefast/breakefast.component';
+import { HistoryComponent } from './history/history.component';
+import { TrackorderComponent } from './trackorder/trackorder.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { BurgersComponent } from './burgers/burgers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : 'home' , component: HomeComponent },
+  {path : 'login' , component: LoginComponent },
+  {path:'breakefast', component: BreakefastComponent},
+  {path:'history', component: HistoryComponent},
+  {path:'profile', component:HistoryComponent},
+  {path:'trackorder', component: TrackorderComponent},
+
+  {path:'drinks', component: DrinksComponent},
+    {path: 'burgers', component: BurgersComponent},
+    
+
+
+    
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { BreakefastComponent } from './breakefast/breakefast.component';
+import { HistoryComponent } from './history/history.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TrackorderComponent } from './trackorder/trackorder.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { BurgersComponent } from './burgers/burgers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    LoginComponent,
+    HomeComponent,
+    BreakefastComponent,
+    HistoryComponent,
+    ProfileComponent,
+    TrackorderComponent,
+    DrinksComponent,
+    BurgersComponent
   ],
   imports: [
     BrowserModule,
