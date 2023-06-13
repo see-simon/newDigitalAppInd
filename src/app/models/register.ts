3ef13c4141a9411bd41c2d624765ba804499b7c0
Your Name <you@example.com>
2023-06-13 07:28:16 +0200

    commit

--- src/app/models/register.ts
+++ src/app/models/register.ts
@@ -5,6 +5,7 @@ export class Register {
     email: number;
     password: string;
 
+    
     constructor(firstname: string, lastname: string, email: number, password: string) {
       this.firstname = firstname;
       this.lastname = lastname;
